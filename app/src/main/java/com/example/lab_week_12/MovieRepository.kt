f5615716319bf5b43eb837c4c179bf89e96be839
Felyks <gamingfelix78@gmail.com>
2024-11-26 12:54:37 +0700

    initial commit

--- app/src/main/java/com/example/lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_12/MovieRepository.kt
@@ -1,7 +1,31 @@
 package com.example.lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.lab_week_12.api.MovieService
+import com.example.lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "6e3b07ee84434c90da023c45e644246c"
 
+    // LiveData that contains a list of movies
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    // LiveData that contains an error message
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    // fetch movies from the API
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
 }
